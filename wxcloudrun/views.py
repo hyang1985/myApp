ae6b6cef65ee0ecf03f525de4bf47682c6cfd985
hy <hyang1985@hotmail.com>
2022-05-31 09:35:30 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -4,6 +4,7 @@ import logging
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import hashlib
 from wxcloudrun.models import Counters
 
