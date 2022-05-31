300e47c012ca03b45bd6f16bc55c00221e78f29c
hy <hyang1985@hotmail.com>
2022-05-31 10:27:21 +0800

    Update urls.py

--- wxcloudrun/urls.py
+++ wxcloudrun/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls import url
 urlpatterns = (
     # 计数器接口
     url(r'^^api/count(/)?$', views.counter),
-    url(r'^$', views.weixin_main, name='weixin_main'),
+    url(r'^$', views.weixin_main),
     url(r'^^test',views.test),
     # 获取主页
     url(r'(/)?$', views.index),
